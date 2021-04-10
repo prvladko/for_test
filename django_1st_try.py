072a5e6a4c9042a8626bc25d261c2d673ebb749a
prvladko <prvladko@gmail.com>
2021-04-10 14:59:44 +0300

    основы ООП

--- django_1st_try.py
+++ django_1st_try.py
@@ -1,4 +1,5 @@
 # args kwargs
+
 def plus(*args):
     result = 0
     for x in args:
@@ -9,3 +10,22 @@ def plus(*args):
 
 summa = plus(1,2,1,1,1,1,2,1,1,12,1,1,1,1)
 print(summa)
+
+# Основы ООП
+
+class Car():
+    wheels = 4
+    doors = 4
+    windows = 4
+    seets = 5
+
+porsche = Car()
+porsche.color = 'black'
+
+ferrari = Car()
+ferrari.color = 'red'
+
+mini = Car()
+mini.color = 'yellow'
+
+print(mini.color)
