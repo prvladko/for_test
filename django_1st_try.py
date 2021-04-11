801539e798acaf47d694841c311a1972de0b4c93
prvladko <prvladko@gmail.com>
2021-04-11 20:15:48 +0300

    Расширение классов (перезапись внутренних функций классов и super)

--- django_1st_try.py
+++ django_1st_try.py
@@ -12,14 +12,17 @@ class Car():
         return f'Автомобиль с {self.wheels} колесами'
 
 class Convertible(Car):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.time_to_take_off = 30
+
     def take_off(self):
         print('Крыша убрана')
 
-class Coupe(Convertible):
-    def somth(self):
-        return
+    def __str__(self):
+        return f'Кабриолет с {self.wheels} колесами и убираемой крышей'
 
 
-porsche = Coupe(color='green', price=100000)
+
+porsche = Convertible(color='green', price=100000)
 print(porsche.wheels)
-porsche.take_off()
