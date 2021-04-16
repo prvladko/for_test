2136afcc7b66788e44996bb5517758ddd1db6359
prvladko <prvladko>
2021-04-16 13:15:03 +0300

    weather parser done

--- weather_bot/prediction.py
+++ weather_bot/prediction.py
@@ -1,2 +1,11 @@
 import requests
 
+def get_prediction(mode='temwind'):
+    r = requests.get('http://wttr.in/moscow?format=4')
+    temp, wind = r.text.split(' ')[2][2:], r.text.split()[3][2:]
+    if mode == 'temp':
+        return temp
+    elif mode == 'wind':
+        return wind
+    elif mode == 'tempwind':
+        return temp, wind
