c7a31b769552a262753ab4eb8551376be976b259
prvladko <prvladko>
2021-04-16 13:43:46 +0300

    add /start & /help

--- weather_bot/easy_bot.py
+++ weather_bot/easy_bot.py
@@ -8,10 +8,13 @@ bot = telebot.TeleBot('1769995853:AAGJ45pcByH14_09sSfHVTKolj8Z7Wl8ibw')
 @bot.message.handler(commands = ['start', 'help'])
 def welcome(message):
     #добавим клавиатуру
-    keyboard = types.InlineKeyboardMarkup()
-    key_temp = types.InlineKeyboardButton(text='Температура', callback_data='temp')
-    keyboard.add(key_temp) #добавляем кнопку в клавиатуру
-    key_wind = types.InlineKeyboardButton(text='Ветер', callback_data='wind')
-    keyboard.add(key_wind)
-    key_tempwind = types.InlineKeyboardButton(text='Температура и Ветер', callback_data='tempwind')
-    keyboard.add(key_tempwind)
+    if message.text == '\start':
+        keyboard = types.InlineKeyboardMarkup()
+        key_temp = types.InlineKeyboardButton(text='Температура', callback_data='temp')
+        keyboard.add(key_temp) #добавляем кнопку в клавиатуру
+        key_wind = types.InlineKeyboardButton(text='Ветер', callback_data='wind')
+        keyboard.add(key_wind)
+        key_tempwind = types.InlineKeyboardButton(text='Температура и Ветер', callback_data='tempwind')
+        keyboard.add(key_tempwind)
+    elif message.text == '\help':
+        bot.send_message(message.from_user.id, 'Пожалуйста, напечатайте \start')
