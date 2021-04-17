e21e511a209deafe3e8eb3f8851546617532fce9
prvladko <prvladko>
2021-04-17 17:34:22 +0300

    pycharm commit

--- weather_bot/easy_bot.py
+++ weather_bot/easy_bot.py
@@ -5,7 +5,7 @@ from prediction import get_prediction
 bot = telebot.TeleBot('1769995853:AAGJ45pcByH14_09sSfHVTKolj8Z7Wl8ibw')
 
 #Метод для обработки комманд
-@bot.message.handler(commands = ['start', 'help'])
+@bot.message_handler(commands = ['start', 'help'])
 def welcome(message):
     #добавим клавиатуру
     if message.text == '\start':
@@ -19,5 +19,11 @@ def welcome(message):
     elif message.text == '\help':
         bot.send_message(message.from_user.id, 'Пожалуйста, напечатайте \start')
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    prediction = get_prediction(call.data)
+    if type(prediction) != str:
+        prediction = prediction[0] + ' и ' + prediction[1]
+    bot.send_message(call.from_user.id, 'В Москве сегодня ' + prediction)
 
 bot.polling()
